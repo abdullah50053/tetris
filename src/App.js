56f03839f2c5c057ef0fd1fefd74fafb4c6ddbbc
Abdullah Hasani <abdullah50053@gmail.com>
2022-01-14 10:30:17 -0600

    Add Google Analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Tetris from './components/Tetris';
+import ReactGA from 'react-ga';
 
+const TRACKING_ID = "299462649"; 
+ReactGA.initialize(TRACKING_ID);
 const App = () => (
   <div className="App">
     <Tetris />
